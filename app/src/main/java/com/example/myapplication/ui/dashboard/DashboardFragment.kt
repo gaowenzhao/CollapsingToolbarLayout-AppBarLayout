8c0f406f3c2372036dff719f634ed981785cbed0
gaowenzhao <510368603@qq.com>
2019-12-14 22:38:50 +0800

    other show

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
@@ -13,6 +13,9 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.myapplication.R
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.appbar
+import kotlinx.android.synthetic.main.fragment_home.toolbar
+import kotlinx.android.synthetic.main.test1.*
 import kotlin.math.abs
 import kotlin.math.roundToInt
 
@@ -27,7 +30,7 @@ class DashboardFragment : Fragment() {
     ): View? {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.test, container, false)
+        val root = inflater.inflate(R.layout.test1, container, false)
       /*  val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(this, Observer {
             textView.text = it
@@ -38,8 +41,15 @@ class DashboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { p0, verticalOffset ->
-            var percent = (abs(verticalOffset * 1.0f) / p0.totalScrollRange)
+            var totalScrollRange = p0.totalScrollRange*2/3
+            var absVerticalOffset = abs(verticalOffset * 1.0f)
+            if(absVerticalOffset>totalScrollRange){
+                absVerticalOffset = totalScrollRange.toFloat()
+            }
+            var percent = (absVerticalOffset / totalScrollRange)
             toolbar.background.alpha = (255 * percent).roundToInt()
+            toolbartitle.alpha = percent
+            lefttitle.alpha = 1-percent
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 Log.i("text","toolbar.background.alpha=${toolbar.background.alpha}")
             }
